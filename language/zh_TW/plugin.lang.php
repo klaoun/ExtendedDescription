dec173036564ba5a2b2eeb00a755325cc1cf60ed
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-03-17 06:04:36 +0100

    Update zh_TW, thanks to closer

--- language/zh_TW/plugin.lang.php
+++ language/zh_TW/plugin.lang.php
@@ -21,14 +21,18 @@
 // | USA.                                                                  |
 // +-----------------------------------------------------------------------+
 $lang['Use Extended Description tags...'] = '使用擴展說明標籤...';
-$lang['This category is redirected to %s'] = '此相簿中將被重定向到 %s';
+$lang['This category is redirected to %s'] = '此相簿中將被重新導向到 %s';
 
 $lang['Multilingual descriptions'] = '多語言描述';
 $lang['Extended descriptions'] = '擴展描述';
 $lang['Insert an album or a photo'] = '插入一個相冊或圖片';
 $lang['Hide elements'] = '隱藏元素';
-$lang['Redirect elements'] = '重定向元素';
+$lang['Redirect elements'] = '重新導向項目';
 $lang['Extended Description have been successfully installed. Now you can use all its features in most text boxes of Piwigo.'] = '擴展說明已成功安裝。現在，您可以使用在Piwigo的大多數文本框裡的所有功能。';
-$lang['Back'] = '後退';
 $lang['Insert a carousel'] = '插入一個幻燈';
 $lang['Login link & logged in block'] = '登錄鏈接&登錄狀態';
+$lang['Carrousel'] = 'Carrousel';
+$lang['Login block'] = '登入區塊';
+$lang['Redirects'] = '重新導向';
+$lang['Multilingual'] = '多國語言';
+$lang['Descriptions'] = '描述';
